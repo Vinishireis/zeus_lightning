98db545539c0fae8c7fdce5d058e5159f1f44385
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-05-02 12:00:27 -0300

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -6,7 +6,7 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req, res) {
-  console.log("Entrou no endpoint")
+  return res.status(200).json({ message: 'Entrou no enpoint' });
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Método não permitido' });
   }
