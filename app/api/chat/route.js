0dacc5b101713adba7513fbc832f18e8dab92567
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-05-02 23:50:26 -0300

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -63,9 +63,9 @@ export async function POST(request) {
       const messageContent = openaiResponse.data.output[0].content[0].text;
       previousResponseId = openaiResponse.data.id;
 
-      if (contador > 0 && contador < 10) {
+      if (contador > 0 && contador < 6) {
         relatorio += '\n\n' + messageContent;
-      }else if(contador == 10){
+      }else if(contador == 5){
         relatorioFinal = messageContent;
       }
     }
